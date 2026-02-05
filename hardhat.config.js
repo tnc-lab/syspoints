0b1afeb2f851f34986b3e2a68cada2dba32d2485
José Nícollas <josenicollassilvadesouza@gmail.com>
2026-02-05 20:44:05 -0300

    fix: add version 0.8.20 as a minimum for smart contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545"
