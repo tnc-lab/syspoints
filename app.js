4cb525de868d2343ed4e50c3ae4e3c94c07480c5
adrian <adrian@nc.net.pe>
2026-02-11 01:58:10 -0500

    fix(docs): serve swagger ui via CDN on /docs

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const { syscoinRouter } = require('./routes/syscoin');
 const { leaderboardRouter } = require('./routes/leaderboard');
 const { authRouter } = require('./routes/auth');
 const { authenticate } = require('./middlewares/auth');
-const swaggerUi = require('swagger-ui-express');
 const { swaggerSpec } = require('./config/swagger');
 const { adminRouter } = require('./routes/admin');
 const { healthRouter } = require('./routes/health');
@@ -20,7 +19,32 @@ app.get('/openapi.json', (req, res) => {
   res.json(swaggerSpec);
 });
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(null, { swaggerUrl: '/openapi.json' }));
+app.get('/docs', (req, res) => {
+  const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
+  const specUrl = `${baseUrl}/openapi.json`;
+  res.setHeader('Content-Type', 'text/html');
+  res.send(`<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Syspoints API Docs</title>
+    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
+  </head>
+  <body>
+    <div id="swagger-ui"></div>
+    <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+    <script>
+      window.ui = SwaggerUIBundle({
+        url: '${specUrl}',
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [SwaggerUIBundle.presets.apis],
+        layout: "BaseLayout"
+      });
+    </script>
+  </body>
+</html>`);
+});
 
 app.get('/', (req, res) => {
   res.redirect('/docs');
